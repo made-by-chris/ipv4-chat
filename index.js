7aec7cab62636bcb3b64d15125fa33dbeeda511c
z-memory <dont@forgetmy.email>
2022-06-24 17:31:23 +0200

    tweak ui

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require('express')
-const app = express()
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
-app.use(express.static("public"))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-io.on('connection', (client) => {
-  client.on('group', function (new_room) {
+io.on("connection", (client) => {
+  client.on("group", function (new_room) {
     client.leave(client.room);
-    client.join(new_room)
+    client.join(new_room);
     client.room = new_room;
   });
 
-  client.on("bzzz", msg => {
-    console.log(client, msg)
+  client.on("bzzz", (msg) => {
     client.broadcast.emit(client.room, msg);
   });
 });
